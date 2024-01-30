47c530f8884deff15b30ac4a38f0105b7034ea6d
rashenko <149801050+rashenko@users.noreply.github.com>
2024-01-30 23:18:01 +0300

    elasticsearch_config.sh

---
+++ elasticsearch_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file_path="/etc/elasticsearch/elasticsearch.yml"
+new_elk='"elk"'
+
+# Замена строк в файле
+sed -i 's/xpack.security.enabled: .*/xpack.security.enabled: false/g' $file_path
+sed -i 's/xpack.security.enrollment.enabled: .*/xpack.security.enrollment.enabled: false/g' $file_path
+sed -i 's/  enabled: .*/  enabled: false/g' $file_path
+sed -i 's/xpack.security.transport.ssl:\n  enabled: .*/xpack.security.transport.ssl:\n  enabled: false/g' $file_path
+sed -i "s/cluster.initial_master_nodes:.*/cluster.initial_master_nodes: [$new_elk]/g" $file_path
+
+# Запуск сервиса
+sudo systemctl daemon-reload
+sudo systemctl enable --now elasticsearch.service
