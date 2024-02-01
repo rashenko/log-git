d6fde677a9d07c3de806a92e05104241109a8dc1
rashenko <149801050+rashenko@users.noreply.github.com>
2024-02-01 21:29:28 +0300

    kibana_script.sh

--- kibana_script.sh
+++ kibana_script.sh
@@ -14,11 +14,11 @@ sed -i 's/#server.host: .*/server.host: "0.0.0.0"/g' $file_path
 sed -i 's/server.host: .*/server.host: "0.0.0.0"/g' $file_path
 
 
-# Запуск сервиса
+# Запуск сервиса Kibana
 systemctl daemon-reload
 
-#Добовляем в автозагрузку Kibana
+#Добовляем в автозагрузку сервис Kibana
 systemctl enable --now kibana.service
 
-#Проверяем статус Kibana
+#Проверяем статус сервиса Kibana
 systemctl status kibana.service
